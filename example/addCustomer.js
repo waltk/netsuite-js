8a6e0174972405a80d43dd8b5d9beb39381e5b19
Alexandr <alexandr@buildateam.io>
2018-02-23 04:49:03 +0300

    add customer (part 4)

--- example/addCustomer.js
+++ example/addCustomer.js
@@ -12,10 +12,10 @@ service
     var address = new NetSuite.Common.Address();
     address.addr1 = 'addr1';
     address.addr2 = 'addr2';
-    address.addr3 = 'addr3';
+    // address.addr3 = 'addr3';
     address.addressee = 'addressee';
     address.addrPhone = 'addrPhone';
-    address.addrText = 'addrText';
+    // address.addrText = 'addrText';
     address.attention = 'attention';
     address.city = 'city';
     address.country = '_unitedStates';
@@ -24,17 +24,39 @@ service
     address.zip	 = '000000';
     var customerAddressbook = new NetSuite.Relationships.CustomerAddressbook();
     customerAddressbook.defaultBilling = true;
+    customerAddressbook.defaultShipping = false;
+    customerAddressbook.isResidential = false;
+    customerAddressbook.label = 'label';
+    customerAddressbook.addressbookAddress = address;
+    customerAddressbookList.addressbook.push(customerAddressbook);
+
+    address = new NetSuite.Common.Address();
+    address.addr1 = 'addr1_2';
+    address.addr2 = 'addr2_2';
+    // address.addr3 = 'addr3';
+    address.addressee = 'addressee_2';
+    address.addrPhone = 'addrPhone_2';
+    // address.addrText = 'addrText';
+    address.attention = 'attention_2';
+    address.city = 'city_2';
+    address.country = '_unitedStates';
+    address.override = false;
+    address.state = 'state_2';
+    address.zip	 = '000002';
+    customerAddressbook = new NetSuite.Relationships.CustomerAddressbook();
+    customerAddressbook.defaultBilling = false;
     customerAddressbook.defaultShipping = true;
     customerAddressbook.isResidential = false;
     customerAddressbook.label = 'label';
     customerAddressbook.addressbookAddress = address;
     customerAddressbookList.addressbook.push(customerAddressbook);
+
     var customer = new NetSuite.Relationships.Customer();
-    customer.firstName = new NetSuite.StringObject('firstName', 'firstName');
-    customer.lastName =  new NetSuite.StringObject('lastName', 'lastName');
-    customer.phone =  new NetSuite.StringObject('phone', '123456789');
-    customer.email =  new NetSuite.StringObject('email', 'joe.doe@abc.com');
-    // customer.addressbookList = customerAddressbookList;
+    customer.firstName = ('firstName');
+    customer.lastName =  ('lastName');
+    customer.phone =  ('123456789');
+    customer.email =  ('joe.doe@abc.com');
+    customer.addressbookList = customerAddressbookList;
     return service.add(customer);
     // return Promise.resolve(customerAddressbookList);
   })
