83ad0d0c57bab02f848f050708a1abf0bb2b1523
Alexandr <alexandr@buildateam.io>
2018-03-02 03:23:38 +0300

    fix

---
+++ example/getShipmentList.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var denodeify = require('denodeify');
+var NetSuite = require('../');
+
+var credentials = require('./credentials.json');
+var config = new NetSuite.Configuration(credentials);
+var service = new NetSuite.Service(config);
+
+console.log('Creating NetSuite connection');
+
+service
+  .init(true /* skipDiscovery */ )
+  .then(function( /*client*/ ) {
+    console.log('WSDL processed');
+    return service.getSelectValue(null);
+  })
+  .then(function(result) {
+    return result;
+  })
+  .catch(function(err) {
+    console.error(err);
+    console.error('Last Request:');
+    console.error(service.config.client.lastRequest);
+  });
